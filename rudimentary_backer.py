0f47247bf6d066778001a13fbac81852f93a861d
LimitsAtInfinity01 <mmddcky@hotmail.com>
2025-10-30 18:53:23 -0600

    refractor code into functions and main function

--- rudimentary_backer.py
+++ rudimentary_backer.py
@@ -6,9 +6,6 @@ from tkinter import filedialog as fd
 from tkinter import messagebox as msg
 
 home_directory = os.path.expanduser('~')
-root = tk.Tk()
-root.title("Backer")
-root.geometry('800x600')
 main_font = "Helvetica"
 main_font_size = 14
 
@@ -16,25 +13,8 @@ file = ''
 save_directory = ''
 files = []
 
-""" THIS HIDES HIDDEN FILES FROM APPEARING IN THE FILE SELECTION WINDOWS """
-""" NOT MY CODE AND I DON'T KNOW HOW OR WHY IT WORKS """
-try:
-    # call a dummy dialog with an impossible option to initialize the file
-    # dialog without really getting a dialog window; this will throw a
-    # TclError, so we need a try...except :
-    try:
-        root.tk.call('tk_getOpenFile', '-foobarbaz')
-    except tk.TclError:
-        pass
-    # now set the magic variables accordingly
-    root.tk.call('set', '::tk::dialog::file::showHiddenBtn', '1')
-    root.tk.call('set', '::tk::dialog::file::showHiddenVar', '0')
-except:
-    pass
-""""""
-
 # It opens a file and adds it to the list of files to be copied
-def open_file():
+def open_file(root):
     global file
     global files
     file = fd.askopenfilename(parent=root, title='Open File', initialdir=home_directory)
@@ -50,11 +30,11 @@ def open_file():
             f_label.config(text=file_name)
             f_label.pack()
 
-def save_file():
+def save_file(root):
     global save_directory
     save_directory = fd.askdirectory(parent=root, title='Save to', initialdir=home_directory)
 
-def back_up():
+def back_up(root):
     global files
     if len(files) > 0 and save_directory:
         for f in files:
@@ -65,17 +45,41 @@ def back_up():
         warning_msg = msg.showwarning(title="Warning", message="Select a file and directory to save it")
         print("File not found")
 
-# File Ope Button
-file_path_btn = tk.Button(root, text="Open File", command=open_file)
-file_path_btn.pack()
+def main():
+    root = tk.Tk()
+    root.title("Backer")
+    root.geometry('800x600')
 
-# Save directory
-save_btn = tk.Button(root, text="Choose destination", command=save_file)
-save_btn.pack()
+    """ THIS HIDES HIDDEN FILES FROM APPEARING IN THE FILE SELECTION WINDOWS """
+    """ NOT MY CODE AND I DON'T KNOW HOW OR WHY IT WORKS """
+    try:
+        # call a dummy dialog with an impossible option to initialize the file
+        # dialog without really getting a dialog window; this will throw a
+        # TclError, so we need a try...except :
+        try:
+            root.tk.call('tk_getOpenFile', '-foobarbaz')
+        except tk.TclError:
+            pass
+        # now set the magic variables accordingly
+        root.tk.call('set', '::tk::dialog::file::showHiddenBtn', '1')
+        root.tk.call('set', '::tk::dialog::file::showHiddenVar', '0')
+    except:
+        pass
+    """"""
 
-# Back up files
-back_up_btn = tk.Button(root, text="Back Files", command=back_up)
-back_up_btn.pack()
+    # File Ope Button
+    file_path_btn = tk.Button(root, text="Open File", command=lambda: open_file(root))
+    file_path_btn.pack()
 
+    # Save directory
+    save_btn = tk.Button(root, text="Choose destination", command=lambda: save_file(root))
+    save_btn.pack()
 
-root.mainloop()
+    # Back up files
+    back_up_btn = tk.Button(root, text="Back Files", command=lambda: back_up(root))
+    back_up_btn.pack()
+
+    root.mainloop()
+
+main()
+
