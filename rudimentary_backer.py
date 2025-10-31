47b433333e05c6498af94c43be6364a3f3dba063
LimitsAtInfinity01 <mmddcky@hotmail.com>
2025-10-30 18:33:24 -0600

    Multiple files to back up support

--- rudimentary_backer.py
+++ rudimentary_backer.py
@@ -1,11 +1,11 @@
 import os
 import shutil
+
 import tkinter as tk
 from tkinter import filedialog as fd
+from tkinter import messagebox as msg
 
 home_directory = os.path.expanduser('~')
-"""Test"""
-print("Test")
 root = tk.Tk()
 root.title("Backer")
 root.geometry('800x600')
@@ -14,9 +14,10 @@ main_font_size = 14
 
 file = ''
 save_directory = ''
+files = []
 
-""" NOT MY CODE AND I DON'T KNOW HOW OR WHY IT WORKS """
 """ THIS HIDES HIDDEN FILES FROM APPEARING IN THE FILE SELECTION WINDOWS """
+""" NOT MY CODE AND I DON'T KNOW HOW OR WHY IT WORKS """
 try:
     # call a dummy dialog with an impossible option to initialize the file
     # dialog without really getting a dialog window; this will throw a
@@ -30,33 +31,39 @@ try:
     root.tk.call('set', '::tk::dialog::file::showHiddenVar', '0')
 except:
     pass
+""""""
 
-# tkinter.filedialog.askopenfile(mode='r', **options)
+# It opens a file and adds it to the list of files to be copied
 def open_file():
     global file
+    global files
     file = fd.askopenfilename(parent=root, title='Open File', initialdir=home_directory)
+    if file:
+        files.append(file)
+
+    f_label = tk.Label(text='')
+    print(len(files))
+    print(files)
+    if len(files) >= 1:
+        for f in files:
+            file_name = f.split('/')[-1]
+            f_label.config(text=file_name)
+            f_label.pack()
 
 def save_file():
     global save_directory
     save_directory = fd.askdirectory(parent=root, title='Save to', initialdir=home_directory)
 
 def back_up():
-    if file:
-        if save_directory:
-            shutil.copy2(file, save_directory)
-        else:
-            print("Wronged Directory")
+    global files
+    if len(files) > 0 and save_directory:
+        for f in files:
+            shutil.copy2(f, save_directory)
+        success_msg = tk.Label(root, text="Back up completed!")
+        success_msg.pack()
     else:
+        warning_msg = msg.showwarning(title="Warning", message="Select a file and directory to save it")
         print("File not found")
-
-    print("Back up completed!")
-    truncated_file = file.split('/')[-1]
-    print(f"File {truncated_file} was save to {save_directory}")
-
-    success_msg = tk.Label(root, text="Back up completed!")
-    success_path_saved_to = tk.Label(root, text=f"File {truncated_file} was save to {save_directory}")
-    success_msg.pack()
-    success_path_saved_to.pack()
 
 # File Ope Button
 file_path_btn = tk.Button(root, text="Open File", command=open_file)
